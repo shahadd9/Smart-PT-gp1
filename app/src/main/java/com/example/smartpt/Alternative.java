e3b51d528439222226cda98a65a1c13f1ca89eaa
shahadd9 <78095199+shahadd9@users.noreply.github.com>
2022-01-30 03:53:52 +0300

    alternative methode semi

--- app/src/main/java/com/example/smartpt/Alternative.java
+++ app/src/main/java/com/example/smartpt/Alternative.java
@@ -1,25 +1,51 @@
 package com.example.smartpt;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.text.format.Formatter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Alternative extends AppCompatActivity {
 
-    private TextView lbl1,exName1,exName2, equ2, back;
+    private FirebaseFirestore db;
+//lbl1,exName1,exName2, equ2, back,
+//    private TextView test1,test2,test3,test4, back;
+    private TextView lbl1,exName1,exName2, equ1,equ2, back;
     private Button exBtn1,exBtn2;
-    private String name, SessionNo,level,exercise1,exercise2, excName1,excName2,ex1String,ex2String;
+    private String name, SessionNo,level,exercise1,eqName1,exercise2, excName1,excName2,ex1String,ex2String,force1,muscle1,force2,muscle2,day,force,muscle,dday,fforce,mmuscle;
     private int index;
     String ex1[] = new String[4];
     String ex2[] = new String[4];
+    String dayAr[]=new String[100];
+    String forceAr[] =new String[100];
+    String muscleAr[] =new String[100];
+
 
 
     @Override
@@ -27,15 +53,41 @@ public class Alternative extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alternative);
 
+        //Strings
+        name="";
+    SessionNo="";
+    level="";
+    exercise1="";
+    eqName1="";
+    exercise2="";
+    excName1="";
+    excName2="";
+    ex1String="";
+    ex2String="";
+    force1="";
+    muscle1="";
+    force2="";
+    muscle2="";
+    day="";
+    force="";
+    muscle="";
+    dday="";
+    fforce="";
+    mmuscle="";
         //declare xml elements
 
         lbl1=findViewById(R.id.lbl1);
         exName1=findViewById(R.id.exName1);
         exName2=findViewById(R.id.exName2);
+        equ1=findViewById(R.id.Equ1);
         equ2=findViewById(R.id.Equ2);
         back=findViewById(R.id.back);
         exBtn1=findViewById(R.id.exbtn1);
         exBtn2=findViewById(R.id.exbtn2);
+//        test1=findViewById(R.id.test1);
+//        test2=findViewById(R.id.test2);
+//        test3=findViewById(R.id.test3);
+//        test4=findViewById(R.id.test4);
 
         //get values from intent
 
@@ -48,8 +100,34 @@ public class Alternative extends AppCompatActivity {
         //////////////////////////////////////////////////
 
         lbl1.setText(name+" Exercise Alternatives:");
+        ////////////////////////////////////////////////////
+        findAlternative(name);
 
-//        findAlternative(name);
+//        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+//        String userIp = Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress());
+//        db = FirebaseFirestore.getInstance();
+//        DocumentReference documentReference = db.collection("userProfile").document(userIp).collection("WorkoutPlan").document(userIp).collection(userIp).document("day"+(index+1));
+//        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+//
+//                day = value.getString("plan");
+//                day = day.substring(2, day.length() - 3);
+//                dayAr = day.split("_");
+//
+//
+//                force = value.getString("force");
+//                force = force.substring(2, force.length() - 3);
+//                forceAr = force.split("_");
+//
+//                muscle = value.getString("muscle");
+//                muscle = muscle.substring(2, muscle.length() - 3);
+//                muscleAr = muscle.split("_");
+//            }
+//        });
+
+        //////////////////////////////////////////////////
+
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +140,67 @@ public class Alternative extends AppCompatActivity {
         });
 
 
+        exBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dday="";
+                fforce="";
+                mmuscle="";
+                dayAr[index]=excName1;
+                forceAr[index]=force1;
+                muscleAr[index]=muscle1;
+                for (int i = 0; i < dayAr.length; i++) {
+                    dday+=dayAr[i]+ "_";
+                    fforce+=forceAr[i]+"_";
+                    mmuscle+=muscleAr[i]+"_";
+                }
+//                test1.setText(dday);
+//                test2.setText(fforce);
+
+                dday="['"+dday+"']";
+                fforce="['"+fforce+"']";
+                mmuscle="['"+mmuscle+"']";
+//                test3.setText(mmuscle);
+//                test1.setText(dday);
+//                test2.setText(fforce);
+
+
+
+
+//                WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+//                String userIp = Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress());
+//                db = FirebaseFirestore.getInstance();
+//                CollectionReference ex = db.collection("userProfile");
+//
+//                String s="day"+(index+1);
+//                Map<String,Object> planEx = new HashMap<>();
+//                planEx.put("plan",dday);
+//                planEx.put("force",fforce);
+//                planEx.put("muscle",mmuscle);
+////        planEx.put("sessionNo",SessionNo);
+////        planEx.put("level",level);
+//
+//
+//
+//                ex.document(userIp).collection("WorkoutPlan").document(userIp).collection(userIp).document(s).set(planEx).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        if(task.isSuccessful()){
+//                            startActivity(new Intent(Alternative.this, Exercise.class));
+//                        }
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+////                        Toast.makeText(LoadPa2.this,"Faild",Toast.LENGTH_SHORT);
+//
+//                    }
+//                });
+//
+//
+//
+            }
+        });
 
     }
 
@@ -73,18 +212,34 @@ public class Alternative extends AppCompatActivity {
         // creating python object
         PyObject pyObj= py.getModule("myscript"); // call the python file
 
-        PyObject alt1 = pyObj.callAttr("findAlternative1",name); // call the alt1 method in python
+        PyObject alt1 = pyObj.callAttr("findAlternative1",name,1); // call the alt1 method in python
         exercise1 = alt1.toString();//retrieve  output
-        /////
-        PyObject alt2 = pyObj.callAttr("findAlternative2",name); // call the alt2 method in python
-        exercise2 = alt2.toString();//retrieve  output
-
-        //force,generalMuscle,name,equipment
-        ex1String=exercise1.substring(2,exercise1.length()-3);
+        ex1String=exercise1.substring(1,exercise1.length()-3);
         ex1= ex1String.split("_");
+        excName1=ex1[0].substring(1,ex1[0].length());
+        force1=ex1[1].substring(3,ex1[1].length());
+        muscle1=ex1[2].substring(3,ex1[2].length());
+        exName1.setText(excName1);
+        PyObject eq1 = pyObj.callAttr("altEqpmnt",excName1); // call the altEqpmnt method in python to return the exercise needed equipment
+        eqName1 = eq1.toString();//retrieve  output
+        equ1.setText(eqName1);
 
-        ex2String=exercise2.substring(2,exercise2.length()-3);
+
+        ///
+        PyObject alt2 = pyObj.callAttr("findAlternative1",name,2); // call the alt2 method in python
+        exercise2 = alt2.toString();//retrieve
+        ex2String=exercise2.substring(1,exercise2.length()-3);
         ex2= ex2String.split("_");
+        excName2=ex2[0].substring(1,ex2[0].length());
+        force2=ex2[1].substring(3,ex2[1].length());
+        muscle2=ex2[2].substring(3,ex2[2].length());
+        exName2.setText(excName2);
+
+
+//        ex2String=exercise2.substring(2,exercise2.length()-3);
+//        ex2= ex2String.split("_");
 
     }
+
+
 }
--- app/src/main/python/myscript.py
+++ app/src/main/python/myscript.py
@@ -25,15 +25,15 @@ data = io.StringIO(dataDetails)
 
 dfDetails = pd.read_csv(data, sep=",")
 
-# print(dfDetails['isneedequipment'])
+# print(df['exercisename'])
+
 
 
 eqEx=pd.DataFrame()
 
-
 planlist = []
 def exercises (bench,barbell,stabilityBall,dumbbell,dipMachine,cableMachine):
-        global eqEx,upper,upper1,upper2,upper3,upper4,lower,lower1,lower2
+        global eqEx,upper,upper1,upper2,upper3,upper4,lower,lower1,lower2,d1,d2,d3,d4,d5
         df.drop(df.iloc[:, 7:67].columns, axis = 1)
 
         NoEq=df.loc[df['isneedequipment'] == 0]
@@ -76,13 +76,15 @@ def exercises (bench,barbell,stabilityBall,dumbbell,dipMachine,cableMachine):
 
 
         upper=eqEx.loc[eqEx['generalmuscle'].isin(['chest','arm','back','shoulders','back,arm'])]
-        upper1=eqEx.loc[eqEx['generalmuscle'].isin(['chest'])]
+        upper1 = d1=eqEx.loc[eqEx['generalmuscle'].isin(['chest'])]
         upper2=eqEx.loc[eqEx['generalmuscle'].isin(['arm'])]
-        upper3=eqEx.loc[eqEx['generalmuscle'].isin(['shoulders'])]
+        upper3=d3=eqEx.loc[eqEx['generalmuscle'].isin(['shoulders'])]
         upper4=eqEx.loc[eqEx['generalmuscle'].isin(['back','back,arm'])]
         lower=eqEx.loc[eqEx['generalmuscle'].isin(['core','legs and glutes'])]
-        lower1=eqEx.loc[eqEx['generalmuscle'].isin(['core'])]
+        lower1=d4=eqEx.loc[eqEx['generalmuscle'].isin(['core'])]
         lower2=eqEx.loc[eqEx['generalmuscle'].isin(['legs and glutes'])]
+        d2=eqEx.loc[eqEx['generalmuscle'].isin(['back','legs and glutes'])]
+        d5=eqEx.loc[eqEx['generalmuscle'].isin(['arm','back,arm'])]
 
 
 
@@ -113,21 +115,54 @@ def fullbody(level):
     global fullbodygeneralmuscle,fullbodyforce,fullbodyname
     ex1=upper1.sample()
     ex2=upper2.sample()
+    if(ex1.iat[0,0] == ex2.iat[0,0] ):
+        fullbody(level)
+    
     ex3=upper3.sample()
+    if(ex3.iat[0,0] == ex2.iat[0,0] or ex3.iat[0,0] == ex1.iat[0,0]):
+        fullbody(level)
+        
     ex4=upper4.sample()
+    if(ex4.iat[0,0] == ex3.iat[0,0] or ex4.iat[0,0] == ex2.iat[0,0] or ex4.iat[0,0] == ex1.iat[0,0]):
+        fullbody(level)
+        
     ex5=upper.sample()
+    if(ex5.iat[0,0] == ex4.iat[0,0] or ex5.iat[0,0] == ex3.iat[0,0] or ex5.iat[0,0] == ex2.iat[0,0] or ex5.iat[0,0] == ex1.iat[0,0]):
+        fullbody(level)
+        
     ex6=lower1.sample()
+    if(ex6.iat[0,0] == ex5.iat[0,0] or ex6.iat[0,0] == ex4.iat[0,0] or ex6.iat[0,0] == ex3.iat[0,0] or ex6.iat[0,0] == ex2.iat[0,0] or ex6.iat[0,0] == ex1.iat[0,0]):
+        fullbody(level)
+        
     ex7=lower2.sample()
+    if(ex7.iat[0,0] == ex6.iat[0,0] or ex7.iat[0,0] == ex5.iat[0,0] or ex7.iat[0,0] == ex4.iat[0,0] or ex7.iat[0,0] == ex3.iat[0,0] or ex7.iat[0,0] == ex2.iat[0,0] or ex7.iat[0,0] == ex1.iat[0,0]):
+        fullbody(level)
+        
     rs=pd.concat([ex7, ex6,ex5, ex4,ex3, ex2,ex1])
 
     if(level=="Intermediate" or level=="Professional"):
         ex8=upper.sample()
+        if(ex8.iat[0,0] == ex7.iat[0,0] or ex8.iat[0,0] == ex6.iat[0,0] or ex8.iat[0,0] == ex5.iat[0,0] or ex8.iat[0,0] == ex4.iat[0,0] or ex8.iat[0,0] == ex3.iat[0,0] or ex8.iat[0,0] == ex2.iat[0,0] or ex8.iat[0,0] == ex1.iat[0,0]):
+            fullbody(level) 
+            
         ex9=upper.sample()
+        if(ex9.iat[0,0] == ex8.iat[0,0] or ex9.iat[0,0] == ex7.iat[0,0] or ex9.iat[0,0] == ex6.iat[0,0] or ex9.iat[0,0] == ex5.iat[0,0] or ex9.iat[0,0] == ex4.iat[0,0] or ex9.iat[0,0] == ex3.iat[0,0] or ex9.iat[0,0] == ex2.iat[0,0] or ex9.iat[0,0] == ex1.iat[0,0]):
+            fullbody(level) 
+            
         ex10=lower.sample()
+        if(ex10.iat[0,0] == ex9.iat[0,0] or ex10.iat[0,0] == ex8.iat[0,0] or ex10.iat[0,0] == ex7.iat[0,0] or ex10.iat[0,0] == ex6.iat[0,0] or ex10.iat[0,0] == ex5.iat[0,0] or ex10.iat[0,0] == ex4.iat[0,0] or ex10.iat[0,0] == ex3.iat[0,0] or ex10.iat[0,0] == ex2.iat[0,0] or ex10.iat[0,0] == ex1.iat[0,0]):
+            fullbody(level) 
+            
         rs=pd.concat([ex10, ex9,ex8,ex7, ex6,ex5, ex4,ex3, ex2,ex1])
+        
     if(level=="Professional"):
         ex11=upper.sample()
+        if(ex11.iat[0,0] == ex10.iat[0,0] or ex11.iat[0,0] == ex9.iat[0,0] or ex11.iat[0,0] == ex8.iat[0,0] or ex11.iat[0,0] == ex7.iat[0,0] or ex11.iat[0,0] == ex6.iat[0,0] or ex11.iat[0,0] == ex5.iat[0,0] or ex11.iat[0,0] == ex4.iat[0,0] or ex11.iat[0,0] == ex3.iat[0,0] or ex11.iat[0,0] == ex2.iat[0,0] or ex11.iat[0,0] == ex1.iat[0,0]):
+            fullbody(level) 
+            
         ex12=lower.sample()
+        if(ex12.iat[0,0] == ex11.iat[0,0] or ex12.iat[0,0] == ex10.iat[0,0] or ex12.iat[0,0] == ex9.iat[0,0] or ex12.iat[0,0] == ex8.iat[0,0] or ex12.iat[0,0] == ex7.iat[0,0] or ex12.iat[0,0] == ex6.iat[0,0] or ex12.iat[0,0] == ex5.iat[0,0] or ex12.iat[0,0] == ex4.iat[0,0] or ex12.iat[0,0] == ex3.iat[0,0] or ex12.iat[0,0] == ex2.iat[0,0] or ex12.iat[0,0] == ex1.iat[0,0]):
+            fullbody(level) 
         rs=pd.concat([ex12,ex11,ex10, ex9,ex8,ex7, ex6,ex5, ex4,ex3, ex2,ex1])
     fullbodyname=rs.copy()
     fullbodyforce=rs.copy()
@@ -143,22 +178,57 @@ def upperbody(level):
     global upperbodyname,upperbodyforce,upperbodygeneralmuscle
     ex1=upper1.sample()
     ex2=upper2.sample()
+    
+    if(ex1.iat[0,0] == ex2.iat[0,0] ):
+      upperbody(level)    
+    
     ex3=upper3.sample()
+    if(ex3.iat[0,0] == ex2.iat[0,0] or ex3.iat[0,0] == ex1.iat[0,0]):
+      upperbody(level)    
+    
     ex4=upper4.sample()
+    if(ex4.iat[0,0] == ex3.iat[0,0] or ex4.iat[0,0] == ex2.iat[0,0] or ex4.iat[0,0] == ex1.iat[0,0]):
+      upperbody(level)
+    
     ex5=upper1.sample()
+    if(ex5.iat[0,0] == ex4.iat[0,0] or ex5.iat[0,0] == ex3.iat[0,0] or ex5.iat[0,0] == ex2.iat[0,0] or ex5.iat[0,0] == ex1.iat[0,0]):
+      upperbody(level)
+    
     ex6=upper2.sample()
+    if(ex6.iat[0,0] == ex5.iat[0,0] or ex6.iat[0,0] == ex4.iat[0,0] or ex6.iat[0,0] == ex3.iat[0,0] or ex6.iat[0,0] == ex2.iat[0,0] or ex6.iat[0,0] == ex1.iat[0,0]):
+      upperbody(level)
+    
     ex7=upper3.sample()
+    if(ex7.iat[0,0] == ex6.iat[0,0] or ex7.iat[0,0] == ex5.iat[0,0] or ex7.iat[0,0] == ex4.iat[0,0] or ex7.iat[0,0] == ex3.iat[0,0] or ex7.iat[0,0] == ex2.iat[0,0] or ex7.iat[0,0] == ex1.iat[0,0]):
+      upperbody(level)    
+
     rs=pd.concat([ex7, ex6,ex5, ex4,ex3, ex2,ex1])
 
     if(level=="Intermediate" or level=="Professional"):
         ex8=upper4.sample()
+        if(ex8.iat[0,0] == ex7.iat[0,0] or ex8.iat[0,0] == ex6.iat[0,0] or ex8.iat[0,0] == ex5.iat[0,0] or ex8.iat[0,0] == ex4.iat[0,0] or ex8.iat[0,0] == ex3.iat[0,0] or ex8.iat[0,0] == ex2.iat[0,0] or ex8.iat[0,0] == ex1.iat[0,0]):
+         upperbody(level)
+        
         ex9=upper.sample()
+        if(ex9.iat[0,0] == ex8.iat[0,0] or ex9.iat[0,0] == ex7.iat[0,0] or ex9.iat[0,0] == ex6.iat[0,0] or ex9.iat[0,0] == ex5.iat[0,0] or ex9.iat[0,0] == ex4.iat[0,0] or ex9.iat[0,0] == ex3.iat[0,0] or ex9.iat[0,0] == ex2.iat[0,0] or ex9.iat[0,0] == ex1.iat[0,0]):
+         upperbody(level) 
+            
         ex10=upper.sample()
+        if(ex10.iat[0,0] == ex9.iat[0,0] or ex10.iat[0,0] == ex8.iat[0,0] or ex10.iat[0,0] == ex7.iat[0,0] or ex10.iat[0,0] == ex6.iat[0,0] or ex10.iat[0,0] == ex5.iat[0,0] or ex10.iat[0,0] == ex4.iat[0,0] or ex10.iat[0,0] == ex3.iat[0,0] or ex10.iat[0,0] == ex2.iat[0,0] or ex10.iat[0,0] == ex1.iat[0,0]):
+         upperbody(level) 
+            
         rs=pd.concat([ex10, ex9,ex8,ex7, ex6,ex5, ex4,ex3, ex2,ex1])
+
     if(level=="Professional"):
         ex11=upper.sample()
+        if(ex11.iat[0,0] == ex10.iat[0,0] or ex11.iat[0,0] == ex9.iat[0,0] or ex11.iat[0,0] == ex8.iat[0,0] or ex11.iat[0,0] == ex7.iat[0,0] or ex11.iat[0,0] == ex6.iat[0,0] or ex11.iat[0,0] == ex5.iat[0,0] or ex11.iat[0,0] == ex4.iat[0,0] or ex11.iat[0,0] == ex3.iat[0,0] or ex11.iat[0,0] == ex2.iat[0,0] or ex11.iat[0,0] == ex1.iat[0,0]):
+            upperbody(level) 
+            
         ex12=upper.sample()
+        if(ex12.iat[0,0] == ex11.iat[0,0] or ex12.iat[0,0] == ex10.iat[0,0] or ex12.iat[0,0] == ex9.iat[0,0] or ex12.iat[0,0] == ex8.iat[0,0] or ex12.iat[0,0] == ex7.iat[0,0] or ex12.iat[0,0] == ex6.iat[0,0] or ex12.iat[0,0] == ex5.iat[0,0] or ex12.iat[0,0] == ex4.iat[0,0] or ex12.iat[0,0] == ex3.iat[0,0] or ex12.iat[0,0] == ex2.iat[0,0] or ex12.iat[0,0] == ex1.iat[0,0]):
+         upperbody(level)     
         rs=pd.concat([ex12,ex11,ex10, ex9,ex8,ex7, ex6,ex5, ex4,ex3, ex2,ex1])
+
     upperbodyname=rs.copy()
     upperbodyforce=rs.copy()
     upperbodygeneralmuscle=rs.copy()
@@ -173,21 +243,55 @@ def lowerbody(level):
     global lowerbodygeneralmuscle,lowerbodyforce,lowerbodyname
     ex1=lower1.sample()
     ex2=lower2.sample()
+    if(ex1.iat[0,0] == ex2.iat[0,0] ):
+        lowerbody(level)
+    
     ex3=lower1.sample()
+    if(ex3.iat[0,0] == ex2.iat[0,0] or ex3.iat[0,0] == ex1.iat[0,0]):
+        lowerbody(level)
+        
     ex4=lower2.sample()
+    if(ex4.iat[0,0] == ex3.iat[0,0] or ex4.iat[0,0] == ex2.iat[0,0] or ex4.iat[0,0] == ex1.iat[0,0]):
+        lowerbody(level)
+        
     ex5=lower1.sample()
+    if(ex5.iat[0,0] == ex4.iat[0,0] or ex5.iat[0,0] == ex3.iat[0,0] or ex5.iat[0,0] == ex2.iat[0,0] or ex5.iat[0,0] == ex1.iat[0,0]):
+        lowerbody(level)
+        
+    
     ex6=lower2.sample()
+    if(ex6.iat[0,0] == ex5.iat[0,0] or ex6.iat[0,0] == ex4.iat[0,0] or ex6.iat[0,0] == ex3.iat[0,0] or ex6.iat[0,0] == ex2.iat[0,0] or ex6.iat[0,0] == ex1.iat[0,0]):
+        lowerbody(level)
+    
     ex7=lower.sample()
+    if(ex7.iat[0,0] == ex6.iat[0,0] or ex7.iat[0,0] == ex5.iat[0,0] or ex7.iat[0,0] == ex4.iat[0,0] or ex7.iat[0,0] == ex3.iat[0,0] or ex7.iat[0,0] == ex2.iat[0,0] or ex7.iat[0,0] == ex1.iat[0,0]):
+        lowerbody(level)
+        
+        
     rs=pd.concat([ex7, ex6,ex5, ex4,ex3, ex2,ex1])
 
     if(level=="Intermediate" or level=="Professional"):
         ex8=lower.sample()
+        if(ex8.iat[0,0] == ex7.iat[0,0] or ex8.iat[0,0] == ex6.iat[0,0] or ex8.iat[0,0] == ex5.iat[0,0] or ex8.iat[0,0] == ex4.iat[0,0] or ex8.iat[0,0] == ex3.iat[0,0] or ex8.iat[0,0] == ex2.iat[0,0] or ex8.iat[0,0] == ex1.iat[0,0]):
+            lowerbody(level) 
+            
         ex9=lower.sample()
+        if(ex9.iat[0,0] == ex8.iat[0,0] or ex9.iat[0,0] == ex7.iat[0,0] or ex9.iat[0,0] == ex6.iat[0,0] or ex9.iat[0,0] == ex5.iat[0,0] or ex9.iat[0,0] == ex4.iat[0,0] or ex9.iat[0,0] == ex3.iat[0,0] or ex9.iat[0,0] == ex2.iat[0,0] or ex9.iat[0,0] == ex1.iat[0,0]):
+            lowerbody(level) 
+            
         ex10=lower.sample()
+        if(ex10.iat[0,0] == ex9.iat[0,0] or ex10.iat[0,0] == ex8.iat[0,0] or ex10.iat[0,0] == ex7.iat[0,0] or ex10.iat[0,0] == ex6.iat[0,0] or ex10.iat[0,0] == ex5.iat[0,0] or ex10.iat[0,0] == ex4.iat[0,0] or ex10.iat[0,0] == ex3.iat[0,0] or ex10.iat[0,0] == ex2.iat[0,0] or ex10.iat[0,0] == ex1.iat[0,0]):
+            lowerbody(level) 
+            
         rs=pd.concat([ex10, ex9,ex8,ex7, ex6,ex5, ex4,ex3, ex2,ex1])
     if(level=="Professional"):
         ex11=lower.sample()
+        if(ex11.iat[0,0] == ex10.iat[0,0] or ex11.iat[0,0] == ex9.iat[0,0] or ex11.iat[0,0] == ex8.iat[0,0] or ex11.iat[0,0] == ex7.iat[0,0] or ex11.iat[0,0] == ex6.iat[0,0] or ex11.iat[0,0] == ex5.iat[0,0] or ex11.iat[0,0] == ex4.iat[0,0] or ex11.iat[0,0] == ex3.iat[0,0] or ex11.iat[0,0] == ex2.iat[0,0] or ex11.iat[0,0] == ex1.iat[0,0]):
+            lowerbody(level) 
+            
         ex12=lower.sample()
+        if(ex12.iat[0,0] == ex11.iat[0,0] or ex12.iat[0,0] == ex10.iat[0,0] or ex12.iat[0,0] == ex9.iat[0,0] or ex12.iat[0,0] == ex8.iat[0,0] or ex12.iat[0,0] == ex7.iat[0,0] or ex12.iat[0,0] == ex6.iat[0,0] or ex12.iat[0,0] == ex5.iat[0,0] or ex12.iat[0,0] == ex4.iat[0,0] or ex12.iat[0,0] == ex3.iat[0,0] or ex12.iat[0,0] == ex2.iat[0,0] or ex12.iat[0,0] == ex1.iat[0,0]):
+            lowerbody(level) 
         rs=pd.concat([ex12,ex11,ex10, ex9,ex8,ex7, ex6,ex5, ex4,ex3, ex2,ex1])
     lowerbodyname=rs.copy()
     lowerbodyforce=rs.copy()
@@ -240,7 +344,7 @@ def fiveDay(level,d):
     fiveDayforce=fiveDayforce.drop(rs.columns[[ 0,1,2,3,4,6,7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61]], axis = 1)
     fiveDaygeneralmuscle=fiveDaygeneralmuscle.drop(rs.columns[[ 0,1,2,3,4,5,7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61]], axis = 1)
    # return rs.to_numpy()
-
+   
 def getfullbodyName():
     str1 = "" 
     
@@ -362,3 +466,72 @@ def getfivedaymuscle():
         str1 += ele+"_"
 
     return str1
+
+# exercises (TRUE,TRUE,TRUE,TRUE,TRUE,TRUE)
+# fiveDay("Intermediate",1)
+
+#Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
+tfidf = TfidfVectorizer(stop_words='english')
+
+#Replace NaN with an empty string
+df['targetedmuscle'] = df['targetedmuscle'].fillna('')
+
+#Construct the required TF-IDF matrix by fitting and transforming the data
+tfidf_matrix = tfidf.fit_transform(df['targetedmuscle'])
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+indices = pd.Series(df.index, index=df['exercisename']).drop_duplicates()
+
+def findAlternative1(exName,num,cosine_sim=cosine_sim):
+    idx = indices[exName]
+
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    sim_scores = sim_scores[1:11]
+
+    ex_indices = [i[0] for i in sim_scores]
+    leng=len(ex_indices)
+
+    ex = np.empty(len(ex_indices), dtype = int)
+
+    for i in range(leng):
+        ex[i]=ex_indices[i]+1
+    if num==1:
+        alt=df.loc[(df['id'].isin(ex))& (df['exercisename']!=exName) & (df['isneedequipment']==1)]
+    else:
+        alt=df.loc[(df['id'].isin(ex))& (df['exercisename']!=exName) & (df['isneedequipment']==0)]
+
+
+    
+    alt=alt[['exercisename','force','generalmuscle']] #,,'bench''dumbbell','barbell','stability ball','dip machine','pull-up bar','cable machine'
+    alt=alt.to_numpy()
+    str1 = ""
+    for ele in alt: 
+
+        str1 += ele+"_"
+
+    return str1
+
+def altEqpmnt(exName):
+    eqList=''
+    dfeq=df.loc[df['exercisename'] == exName]
+    dfeq=dfeq[['bench','dumbbell','barbell','stability ball','dip machine','pull-up bar','cable machine']]
+    dfeq=dfeq.values.tolist()
+    if dfeq[0][0]==1:
+       return('Bench')
+    elif dfeq[0][1]==1:
+       eqList=eqList+'Dumbbell'
+    elif dfeq[0][2]==1:
+       eqList=eqList+'Barbell'
+    elif dfeq[0][3]==1:
+       eqList=eqList+'Stability Ball'
+    elif dfeq[0][4]==1:
+       eqList=eqList+'Dip Machine'
+    elif dfeq[0][5]==1:
+       eqList=eqList+'Pull-up Bar'
+    elif dfeq[0][6]==1:
+       eqList=eqList+'Cable Machine'
+    else:
+        eqList= 'No equipment'
+    return eqList
