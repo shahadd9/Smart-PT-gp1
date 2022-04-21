a2a4bc17f70435663af2a3e257dc8b07a36b42a8
shahadd9 <78095199+shahadd9@users.noreply.github.com>
2022-04-21 06:45:08 +0300

    Progress table

--- app/src/main/python/ss.py
+++ app/src/main/python/ss.py
@@ -4,9 +4,15 @@ import pandas as pd
 from os.path import dirname, join
 from io import StringIO
 import numpy as np
+import sklearn
 
 
 
-inst = pd.read_csv("instructions.csv")
 
-inst
+filename = join(dirname(__file__), "progress.csv")
+with open(filename, 'r', encoding='utf8', errors="ignore") as fin:
+    data1=fin.read()
+data = io.StringIO(data1)
+df = pd.read_csv(data, sep=",") # here i read our dataset as dataframe, you can use it as normal
+
+def saveProgress(exName):
